ca9f32374bdc896102b589c45362c7f4ffda9b7c
brinkqiang <brink.qiang@gmail.com>
2023-12-12 19:19:57 +0800

    add

--- example/openssl_rsa_test/dmrsa.h
+++ example/openssl_rsa_test/dmrsa.h
@@ -6,31 +6,46 @@
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
 #include <string>
+#include <vector>
 
 class CDMRSAEncryptor {
 public:
     CDMRSAEncryptor() {
-        rsa = RSA_generate_key(2048, RSA_F4, nullptr, nullptr);
+        rsa = RSA_new();
+        BIGNUM* bne = BN_new();
+        BN_set_word(bne, RSA_F4);
+        if (RSA_generate_key_ex(rsa, 2048, bne, nullptr) != 1) {
+            throw std::runtime_error("RSA key generation failed");
+        }
+        BN_free(bne);
     }
 
     ~CDMRSAEncryptor() {
         RSA_free(rsa);
     }
 
-    std::string publicEncrypt(const std::string& plain_text) {
-        unsigned char encrypted[256] = {0};
-        int encrypted_len = RSA_public_encrypt(plain_text.size(), reinterpret_cast<const unsigned char *>(plain_text.c_str()), encrypted, rsa, RSA_PKCS1_PADDING);
-        return std::string(reinterpret_cast<char*>(encrypted), encrypted_len);
+    std::vector<unsigned char> publicEncrypt(const std::string& plain_text) {
+        std::vector<unsigned char> encrypted(RSA_size(rsa));
+        int encrypted_len = RSA_public_encrypt(plain_text.size(), reinterpret_cast<const unsigned char*>(plain_text.c_str()), encrypted.data(), rsa, RSA_PKCS1_PADDING);
+        if (encrypted_len == -1) {
+            throw std::runtime_error("RSA public encryption failed");
+        }
+        encrypted.resize(encrypted_len);
+        return encrypted;
     }
 
-    std::string privateDecrypt(const std::string& encrypted_text) {
-        unsigned char decrypted[256] = {0};
-        int decrypted_len = RSA_private_decrypt(encrypted_text.size(), reinterpret_cast<const unsigned char *>(encrypted_text.c_str()), decrypted, rsa, RSA_PKCS1_PADDING);
-        return std::string(reinterpret_cast<char*>(decrypted), decrypted_len);
+    std::string privateDecrypt(const std::vector<unsigned char>& encrypted_text) {
+        std::vector<unsigned char> decrypted(RSA_size(rsa));
+        int decrypted_len = RSA_private_decrypt(encrypted_text.size(), encrypted_text.data(), decrypted.data(), rsa, RSA_PKCS1_PADDING);
+        if (decrypted_len == -1) {
+            throw std::runtime_error("RSA private decryption failed");
+        }
+        decrypted.resize(decrypted_len);
+        return std::string(decrypted.begin(), decrypted.end());
     }
 
 private:
-    RSA *rsa;
+    RSA* rsa;
 };
 
 #endif // __DMRSA_H__
--- example/openssl_rsa_test/openssl_rsa_test.cc
+++ example/openssl_rsa_test/openssl_rsa_test.cc
@@ -1,16 +1,20 @@
-
 #include "dmrsa.h"
 
 int main() {
-    CDMRSAEncryptor rsaEncryptor;
+    try {
+        CDMRSAEncryptor rsaEncryptor;
 
-    std::string plain_text = "Hello, RSA!";
-    std::string encrypted_text = rsaEncryptor.publicEncrypt(plain_text);
-    std::string decrypted_text = rsaEncryptor.privateDecrypt(encrypted_text);
+        std::string plain_text = "Hello, RSA!";
+        std::vector<unsigned char> encrypted_text = rsaEncryptor.publicEncrypt(plain_text);
+        std::string decrypted_text = rsaEncryptor.privateDecrypt(encrypted_text);
 
-    std::cout << "加密前：" << plain_text << std::endl;
-    std::cout << "加密后：" << encrypted_text << std::endl;
-    std::cout << "解密后：" << decrypted_text << std::endl;
+        std::cout << "加密前：" << plain_text << std::endl;
+        std::cout << "加密后：" << std::string(encrypted_text.begin(), encrypted_text.end()) << std::endl;
+        std::cout << "解密后：" << decrypted_text << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 
     return 0;
 }
