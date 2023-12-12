0d3edf147b21f7d3fe5104558fc29ac32a58ea0a
brinkqiang <brink.qiang@gmail.com>
2023-12-12 19:27:02 +0800

    add

--- example/openssl_rsa_test/openssl_rsa_test.cc
+++ example/openssl_rsa_test/openssl_rsa_test.cc
@@ -9,9 +9,9 @@ int main() {
         std::vector<unsigned char> encrypted_text = rsaEncryptor.publicEncrypt(plain_text);
         std::string decrypted_text = rsaEncryptor.privateDecrypt(encrypted_text);
 
-        std::cout << "加密前：" << plain_text << std::endl;
-        std::cout << "加密后：" << std::string(encrypted_text.begin(), encrypted_text.end()) << std::endl;
-        std::cout << "解密后：" << decrypted_text << std::endl;
+        std::cout << "Before encryption: " << plain_text << std::endl;
+        std::cout << "After encryption: " << std::string(encrypted_text.begin(), encrypted_text.end()) << std::endl;
+        std::cout << "After decryption: " << decrypted_text << std::endl;
     }
     catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
